2dfb6f50237e355e80538abd6e4484bbe2346a49
Michael <Magawrych85@gmail.com>
2024-09-15 21:08:13 -0400

    init

---
+++ script.js
@@ -0,0 +1,8 @@
+// Part 1: Thinking Functionally
+
+// Take an array of numbers and return the sum.
+// Take an array of numbers and return the average.
+// Take an array of strings and return the longest string.
+// Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
+// For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
+// Take a number, n, and print every number between 1 and n without using loops. Use recursion.
